5536f9731971f71c9e22653fd2df90609fb2a941
Jacob Sanders <sanders.jms@gmail.com>
2025-04-22 21:34:11 -0600

    defined create action and corresponding post_params method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
   def index
+    @posts = Post.all
   end
 
   def new
@@ -8,5 +9,18 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path, notice: "Post was successfully created"
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :body ])
   end
 end
